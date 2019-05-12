f4507268d33d792464928d0912a5b9f1479603b0
Andi <andreas.s.faust@gmail.com>
2019-05-12 05:46:20 +0200

    added case to font info

--- AnagramDrawer.py
+++ AnagramDrawer.py
@@ -18,6 +18,7 @@ class AnagramDrawer:
         self.fonts = json.load(open(font_json_path,'r'))
         self.load_fonts()
         self.current_font = 0
+        self.case = "camel"
         
     def load_fonts(self):
         for f in self.fonts:
@@ -61,14 +62,23 @@ class AnagramDrawer:
                     self.drawArrow(drawer,t1[1],t2[1])
                     t2[2] = True
                     break
+
+    def remove_all_but(self,text,ok_chars):
+        ret = ""
+        for c in text:
+            if(c in ok_chars):
+                ret += c
+        return ret
                 
     def set_font_from_keywords(self,str1,str2):
         sel_font = "mono_01.ttf"
         for f in self.fonts:
-            if(not set(self.fonts[f]["keywords"]).isdisjoint((str(str1 + " " + str2).lower()).split(" "))):
+            compstr = str(str1 + " " + str2).lower()
+            compstr = self.remove_all_but(compstr,"abcdefghijklmnopqrstuvwxyz ")
+            if(not set(self.fonts[f]["keywords"]).isdisjoint(compstr.split(" "))):
                 sel_font = f
                 break
-        
+        self.case = self.fonts[sel_font]["case"]
         self.arrow_y_padding = self.fonts[sel_font]["arrow_y_padding"] * self.aa_factor
         self.arrow_width = self.fonts[sel_font]["arrow_width"] * self.aa_factor
         self.offset =  self.fonts[sel_font]["offset"]
@@ -76,10 +86,23 @@ class AnagramDrawer:
         self.offset[1] *= self.aa_factor
         self.current_font = self.fonts[sel_font]["font"]
 
+    def adjust_case(text):
+        ret = "" 
+        str_arr = text.split(" ")
+        for word in str_arr:
+            if(self.case == "camel"):
+                ret += word[0].upper() + word[1:]
+            if(self.case == "upper"):
+                ret += word.upper()
+            if(self.case == "lower"):
+                ret += word.lower()
+        return ret
+
     #takes two strings and makes an anagram picture from them
     def draw_image(self,str1,str2,filename):
         self.set_font_from_keywords(str1,str2)
-            
+        str1 = self.adjust_case(str1)
+        str2 = self.adjust_case(str2)
         ts1 = self.textSize(str1)
         ts2 = self.textSize(str2)
         imgSize = (max(ts1[0],ts2[0]) + self.edge_padding * 2,ts2[1] + self.y_space + self.edge_padding * 2)
